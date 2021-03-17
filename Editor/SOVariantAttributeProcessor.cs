163df121840b29a0207adf6fb8fd0f6c6bdcf0da
Giezi <bob.giezi@gmail.com>
2021-03-17 08:07:29 +0100

    cleanup

--- Editor/SOVariantAttributeProcessor.cs
+++ Editor/SOVariantAttributeProcessor.cs
@@ -28,7 +28,6 @@ namespace Giezi.Tools
 {
     public class SOVariantAttributeProcessor<T> : OdinPropertyProcessor<T> where T : ScriptableObject
     {
-        // private List<CheckBoxAttribute> _checkBoxAttributes;
         private bool _selectionChangedFlag = false;
         private SOVariant<T> _soVariant = null;
 
@@ -42,7 +41,6 @@ namespace Giezi.Tools
             Debug.Log("got here2");
             
             _soVariant._overridden = null;
-            // _checkBoxAttributes = new List<CheckBoxAttribute>();
             Property.RefreshSetup();
         }
     
@@ -60,12 +58,6 @@ namespace Giezi.Tools
 
             if (_soVariant == null || _soVariant._overridden == null || _soVariant._import == null || _soVariant._children == null){
                 _soVariant = new SOVariant<T>((T)Property.Tree.UnitySerializedObject.targetObject);
-                // if (_soVariant._overridden == null || _soVariant._import == null || _soVariant._children == null)
-                // {
-                //     _soVariant._overridden = null;
-                // _checkBoxAttributes = new List<CheckBoxAttribute>();
-
-                // _soVariant.LoadData(Property.Tree.UnitySerializedObject.targetObject);
 
                 BoxGroupAttribute bxa = new BoxGroupAttribute("Scriptable Object Variant", true, false, 2);
 
@@ -84,11 +76,9 @@ namespace Giezi.Tools
                             new CheckBoxAttribute(propertyInfo.GetMemberInfo().Name,
                                 _soVariant._overridden.Contains(propertyInfo.GetMemberInfo().Name),
                                 _soVariant._target, _soVariant._parent, _soVariant.NotifyOverrideChangeInState);
-                        // _checkBoxAttributes.Add(checkBoxAttribute);
                         propertyInfo.GetEditableAttributesList().Add(checkBoxAttribute);
                         propertyInfo.GetEditableAttributesList().Add(bxa);
                         
-                        // propertyInfo.GetMemberInfo().Module.GetField(propertyInfo.PropertyName).
                         // ! enable to debug
                         // propertyInfo.GetEditableAttributesList().Add(new ShowDrawerChainAttribute());
                     }
@@ -112,11 +102,6 @@ namespace Giezi.Tools
         {
             Selection.selectionChanged -= OnSelectionChanged;
             _selectionChangedFlag = false;
-            
-            // List<string> overriddenMembers = new List<string>();
-            // if (_checkBoxAttributes.Count > 0)
-            //     overriddenMembers = _checkBoxAttributes.Where(attribute => attribute.IsOverriden)
-            //         .Select(attribute => attribute.Name).ToList();
             
             _soVariant.SaveData(_soVariant._overridden);
         }
@@ -176,24 +161,6 @@ public class CheckBoxDrawer : OdinAttributeDrawer<CheckBoxAttribute>
             this.Attribute.NotifyOverrideChange(this.Attribute.Name, newIsOverriden);
             this.Attribute.IsOverriden = newIsOverriden;
         }
-        
-        // if (!this.Attribute.IsOverriden)
-        // {
-        //     var parentValue = parentFieldInfo.GetValue(Attribute.Parent);
-        //     if(targetFieldInfo.GetValue(Attribute.Target) != parentValue)
-        //         targetFieldInfo.SetValue(Attribute.Target, parentValue);
-        // }
-        // else
-        // {
-        //     // handle copy of list/arrays
-        //     if (typeof(IEnumerable).IsAssignableFrom(targetFieldInfo.FieldType) && targetFieldInfo.GetValue(Attribute.Target) == parentFieldInfo.GetValue(Attribute.Parent))
-        //     {
-        //         object parentObject = parentFieldInfo.GetValue(Attribute.Parent);
-        //         byte[] parentAsData = SerializationUtility.SerializeValueWeak(parentObject, DataFormat.Binary);
-        //         object parentObjectCopy = SerializationUtility.DeserializeValueWeak(parentAsData, DataFormat.Binary);
-        //         targetFieldInfo.SetValue(Attribute.Target, parentObjectCopy);
-        //     }
-        // }
         
         GUIContent noLabel = new GUIContent(label);
         noLabel.text = "";
