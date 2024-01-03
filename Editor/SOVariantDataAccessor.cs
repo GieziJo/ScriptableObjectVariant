bd30d67d5e8a6f998c90bb5703352dddf1b48461
Bob Giezi <bob.giezi@gmail.com>
2024-01-03 16:17:39 +0100

    added dependency for old code for upgrade, removed unused imports

--- Editor/SOVariantDataAccessor.cs
+++ Editor/SOVariantDataAccessor.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using Sirenix.OdinInspector;
 using UnityEditor;
 
 namespace Giezi.Tools
--- Editor/UpgradeSOVariantHelper.cs
+++ Editor/UpgradeSOVariantHelper.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using UnityEditor;
 using UnityEngine;
 
